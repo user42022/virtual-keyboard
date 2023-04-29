4e9adf5a6dcec3baeec6c630f6075627f8c407c0
user42022 <andrekizilov@yandex.ru>
2023-04-30 02:08:59 +0300

    feat: create key object

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+const textarea = document.querySelector('textarea');
+const keyList = document.querySelectorAll('div.key');
+
+keyList.forEach((element) => {
+  element.addEventListener('click', () => {
+    const start = textarea.selectionStart;
+    textarea.value = textarea.value.slice(0, start)
+    + element.textContent + textarea.value.slice(start);
+    textarea.selectionStart = start + 1;
+  });
+});
+*/
---
+++ keyboard-object.js
@@ -0,0 +1,188 @@
+export default {
+  backQuote: {
+    code: 'Backquote', styleList: ['key'], content: ['ё', 'Ё', '`', '~'],
+  },
+  digitOne: {
+    code: 'Digit1', styleList: ['key'], content: ['1', '!', '1', '!'],
+  },
+  digitTwo: {
+    code: 'Digit2', styleList: ['key'], content: ['2', '"', '2', '@'],
+  },
+  digitThree: {
+    code: 'Digit3', styleList: ['key'], content: ['3', '№', '3', '#'],
+  },
+  digitFour: {
+    code: 'Digit4', styleList: ['key'], content: ['4', ';', '4', '$'],
+  },
+  digitFive: {
+    code: 'Digit5', styleList: ['key'], content: ['5', '%', '5', '%'],
+  },
+  digitSix: {
+    code: 'Digit6', styleList: ['key'], content: ['6', ':', '6', '^'],
+  },
+  digitSeven: {
+    code: 'Digit7', styleList: ['key'], content: ['7', '?', '7', '&'],
+  },
+  digitEight: {
+    code: 'Digit8', styleList: ['key'], content: ['8', '*', '8', '*'],
+  },
+  digitNine: {
+    code: 'Digit9', styleList: ['key'], content: ['9', '(', '9', '('],
+  },
+  digitZero: {
+    code: 'Digit0', styleList: ['key'], content: ['0', ')', '0', ')'],
+  },
+  backspace: {
+    code: 'Backspace', styleList: ['key', 'key_service', 'key_double-width'], content: ['Back space'],
+  },
+  tab: {
+    code: 'Tab', styleList: ['key', 'key_service'], content: ['Tab'],
+  },
+  keyQ: {
+    code: 'KeyQ', styleList: ['key'], content: ['й', 'Й', 'q', 'Q'],
+  },
+  keyW: {
+    code: 'KeyW', styleList: ['key'], content: ['ц', 'Ц', 'w', 'W'],
+  },
+  keyE: {
+    code: 'KeyE', styleList: ['key'], content: ['у', 'У', 'e', 'E'],
+  },
+  keyR: {
+    code: 'KeyR', styleList: ['key'], content: ['к', 'К', 'r', 'R'],
+  },
+  keyT: {
+    code: 'KeyT', styleList: ['key'], content: ['е', 'Е', 't', 'T'],
+  },
+  keyY: {
+    code: 'KeyY', styleList: ['key'], content: ['н', 'Н', 'y', 'Y'],
+  },
+  keyU: {
+    code: 'KeyU', styleList: ['key'], content: ['г', 'Г', 'u', 'U'],
+  },
+  keyI: {
+    code: 'KeyI', styleList: ['key'], content: ['ш', 'Ш', 'i', 'I'],
+  },
+  keyO: {
+    code: 'KeyO', styleList: ['key'], content: ['щ', 'Щ', 'o', 'O'],
+  },
+  keyP: {
+    code: 'KeyP', styleList: ['key'], content: ['з', 'З', 'p', 'P'],
+  },
+  bracketLeft: {
+    code: 'BracketLeft', styleList: ['key'], content: ['х', 'Х', '[', '{'],
+  },
+  bracketRight: {
+    code: 'BracketRight', styleList: ['key'], content: ['ъ', 'Ъ', ']', '}'],
+  },
+  backSlash: {
+    code: 'BackSlash', styleList: ['key'], content: ['\\', '/', '\\', '|'],
+  },
+  delete: {
+    code: 'Delete', styleList: ['key', 'key_service'], content: ['Del'],
+  },
+  capsLock: {
+    code: 'CapsLock', styleList: ['key', 'key_service', 'key_double-width'], content: ['Caps Lock'],
+  },
+  keyA: {
+    code: 'KeyA', styleList: ['key'], content: ['ф', 'Ф', 'a', 'A'],
+  },
+  keyS: {
+    code: 'KeyS', styleList: ['key'], content: ['ы', 'Ы', 's', 'S'],
+  },
+  keyD: {
+    code: 'KeyD', styleList: ['key'], content: ['в', 'В', 'd', 'D'],
+  },
+  keyF: {
+    code: 'KeyF', styleList: ['key'], content: ['а', 'А', 'f', 'F'],
+  },
+  keyG: {
+    code: 'KeyG', styleList: ['key'], content: ['п', 'П', 'g', 'G'],
+  },
+  keyH: {
+    code: 'KeyH', styleList: ['key'], content: ['р', 'Р', 'h', 'H'],
+  },
+  keyJ: {
+    code: 'KeyJ', styleList: ['key'], content: ['о', 'О', 'j', 'J'],
+  },
+  keyK: {
+    code: 'KeyK', styleList: ['key'], content: ['л', 'Л', 'k', 'K'],
+  },
+  keyL: {
+    code: 'KeyL', styleList: ['key'], content: ['д', 'Д', 'l', 'L'],
+  },
+  semicolon: {
+    code: 'Semicolon', styleList: ['key'], content: ['ж', 'Ж', ';', ':'],
+  },
+  quote: {
+    code: 'Quote', styleList: ['key'], content: ['э', 'Э', '\'', '"'],
+  },
+  enter: {
+    code: 'Enter', styleList: ['key', 'key_service', 'key_double-width'], content: ['Enter'],
+  },
+  shiftLeft: {
+    code: 'ShiftLeft', styleList: ['key', 'key_service', 'key_double-width'], content: ['Shift'],
+  },
+  keyZ: {
+    code: 'KeyZ', styleList: ['key'], content: ['я', 'Я', 'z', 'Z'],
+  },
+  keyX: {
+    code: 'KeyX', styleList: ['key'], content: ['ч', 'Ч', 'x', 'X'],
+  },
+  keyC: {
+    code: 'KeyC', styleList: ['key'], content: ['с', 'С', 'c', 'C'],
+  },
+  keyV: {
+    code: 'KeyV', styleList: ['key'], content: ['м', 'М', 'v', 'V'],
+  },
+  keyB: {
+    code: 'KeyB', styleList: ['key'], content: ['и', 'И', 'b', 'B'],
+  },
+  keyN: {
+    code: 'KeyN', styleList: ['key'], content: ['т', 'Т', 'n', 'N'],
+  },
+  keyM: {
+    code: 'KeyM', styleList: ['key'], content: ['ь', 'Ь', 'm', 'M'],
+  },
+  comma: {
+    code: 'Comma', styleList: ['key'], content: ['б', 'Б', ',', '<'],
+  },
+  period: {
+    code: 'Period', styleList: ['key'], content: ['ю', 'Ю', '.', '>'],
+  },
+  slash: {
+    code: 'Slash', styleList: ['key'], content: ['.', ',', '/', '?'],
+  },
+  arrowUp: {
+    code: 'ArrowUp', styleList: ['key', 'key_service'], content: ['↑'],
+  },
+  shiftRight: {
+    code: 'ShiftRight', styleList: ['key', 'key_service', 'key_double-width'], content: ['Shift'],
+  },
+  controlLeft: {
+    code: 'ControlLeft', styleList: ['key', 'key_service'], content: ['Ctrl'],
+  },
+  metaLeft: {
+    code: 'MetaLeft', styleList: ['key', 'key_service'], content: ['Win'],
+  },
+  altLeft: {
+    code: 'AltLeft', styleList: ['key', 'key_service'], content: ['Alt'],
+  },
+  space: {
+    code: 'Space', styleList: ['key', 'key_space'], content: [''],
+  },
+  altRight: {
+    code: 'AltRight', styleList: ['key', 'key_service'], content: ['Alt'],
+  },
+  arrowLeft: {
+    code: 'ArrowLeft', styleList: ['key', 'key_service'], content: ['←'],
+  },
+  arrowDown: {
+    code: 'ArrowDown', styleList: ['key', 'key_service'], content: ['↓'],
+  },
+  arrowRight: {
+    code: 'ArrowRight', styleList: ['key', 'key_service'], content: ['→'],
+  },
+  controlRight: {
+    code: 'ControlRight', styleList: ['key', 'key_service'], content: ['Ctrl'],
+  },
+};
